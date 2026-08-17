3c9e92e446a209dcbf628bdffebe6357636d18ed
주재상 <joojeasang@gmail.com>
2026-08-17 11:54:16 +0900

    feat: register Bible quiz cleanup trigger

--- functions/main.js
+++ functions/main.js
@@ -1,3 +1,4 @@
 /* Cloud Functions aggregator: existing functions + Bible quiz. */
 Object.assign(exports, require('./index.js'));
 Object.assign(exports, require('./bible-quiz.js'));
+Object.assign(exports, require('./bible-quiz-cleanup.js'));
